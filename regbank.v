ec47528b6c25cb6dc90d00273ed8d89c7ac7dc01
FHejazi <f.hejazi_96@yahoo.com>
2018-12-09 14:35:52 +0330

    regbank.v correction

--- regbank.v
+++ regbank.v
@@ -1,21 +1,20 @@
-module regbank(DataA, DataB, DataC, a, b, c, w, Clk);
+module regbank(DataA, DataB, DataC, AddrA, AddrB, AddrC, w, Clk);
   output [63:0] DataA, DataB;
   input [63:0] DataC;
-  input [4:0] a, b, c;
+  input [4:0] AddrA, AddrB, AddrC;
   input w;
   input Clk;
 
   reg [63:0] registers [31:0];
 
-  assign DataA = registers[a];
-  assign DataB = registers[b];
+  assign DataA = registers[AddrA];
+  assign DataB = registers[AddrB];
 
   always @ (posedge Clk) 
   begin
     if(w)
-      if (c != 5'd31)
-        registers[c] <= DataC;
-      else
-        registers[c] <= 'd0;
+      if (AddrC <= 5'd30)
+        //address 0 to 30
+        registers[AddrC] <= DataC;
   end
 endmodule
