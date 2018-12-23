dd45c8a9c0070eea48629fff9210dfa4e6fb5653
Lsaeedy98 <leila.saeedy1995@gmail.com>
2018-12-23 10:44:49 +0330

    edited controller

--- Controller.v
+++ Controller.v
@@ -1,6 +1,6 @@
 module Controller(insOp,ALUOp,AluSrc,Branch,MemRead,MemWrite,RegWrite,MemtoReg,Reg2Loc);
 	input [10: 0] insOp;
-	output [1:0]ALUOp;
+	output reg [1:0]ALUOp;
 	output reg AluSrc;
 	output reg Branch;
 	output reg MemRead;
@@ -11,24 +11,24 @@ module Controller(insOp,ALUOp,AluSrc,Branch,MemRead,MemWrite,RegWrite,MemtoReg,R
 
 	always@(insOp)
 	begin	
-		if(insOp=11'b1xx0101x000)
+		if(insOp==11'b1xx0101x000)
 			begin 
 			//R-Type
 				 Reg2Loc = 1'b0;
-			  	 ALUSrc = 1'b0;
-			  	 MemToReg = 1'b0;
+			  	 AluSrc = 1'b0;
+			  	 MemtoReg = 1'b0;
 			  	 RegWrite = 1'b1;
 				 MemRead = 1'b0;
 			  	 MemWrite = 1'b0;
 				 Branch = 1'b0;
 				 ALUOp = 2'b10;
 			end
-		else if(insOp=11'b11111000010)
+		else if(insOp==11'b11111000010)
 			begin				
 			//LDUR
 				 Reg2Loc = 1'bx;
-			  	 ALUSrc = 1'b1;
-			  	 MemToReg = 1'b1;
+			  	 AluSrc = 1'b1;
+			  	 MemtoReg = 1'b1;
 			  	 RegWrite = 1'b1;
 				 MemRead = 1'b1;
 			  	 MemWrite = 1'b0;
@@ -36,24 +36,24 @@ module Controller(insOp,ALUOp,AluSrc,Branch,MemRead,MemWrite,RegWrite,MemtoReg,R
 				 ALUOp = 2'b00;
 			end
 			
-		else if(insOp=11'b11111000000)
+		else if(insOp==11'b11111000000)
 			begin
 			//STUR
 				 Reg2Loc = 1'b1;
-			  	 ALUSrc = 1'b1;
-			  	 MemToReg = 1'bx;
+			  	 AluSrc = 1'b1;
+			  	 MemtoReg = 1'bx;
 			  	 RegWrite = 1'b0;
 				 MemRead = 1'b0;
 			  	 MemWrite = 1'b1;
 				 Branch = 1'b0;
 				 ALUOp = 2'b10;
 			end
-		else if(insOp=11'b10110100xxx)
+		else if(insOp==11'b10110100xxx)
 			 begin 
 			// CBZ
 				 Reg2Loc = 1'b1;
-			  	 ALUSrc = 1'b0;
-			  	 MemToReg = 1'bx;
+			  	 AluSrc = 1'b0;
+			  	 MemtoReg = 1'bx;
 			  	 RegWrite = 1'b0;
 				 MemRead = 1'b0;
 			  	 MemWrite = 1'b0;
