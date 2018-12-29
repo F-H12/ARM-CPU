28b5d58f9e6fc10355ad31be49c9cbfd4a2a6b9a
Lsaeedy98 <leila.saeedy1995@gmail.com>
2018-12-29 16:35:14 +0330

    edited size and paramters

--- ALU.v
+++ ALU.v
@@ -1,21 +1,23 @@
-module ALU(input1,input2,OP,ALUOutput,zero);
-  input [63:0]input1; 
-  input [63:0]input2; 
+module ALU
+#(parameter n=64, parameter delay=100)
+(input1,input2,OP,ALUOutput,zero);
+  input [n-1:0]input1; 
+  input [n-1:0]input2; 
   input [3:0]OP;
   output reg [63:0]ALUOutput;
   output zero;
 
   always@(input1,input2,OP)
   begin
+    ALUOutput = 64'bz;
     case(OP)
 
     4'b0000: ALUOutput = input1&input2;
     4'b0001: ALUOutput = input1|input2;
     4'b0010: ALUOutput = input1+input2;
     4'b0110: ALUOutput = input1-input2;
-    4'b0111: ALUOutput = input1;
-    4'b1100: ALUOutput = ~(input1|input2) ;
- //   default: ALUOutput = 
+    4'b0111: ALUOutput = input2;
+    4'b1100: ALUOutput = ~(input1|input2) ; 
     endcase
 
   end
--- Controller.v
+++ Controller.v
@@ -1,4 +1,6 @@
-module Controller(insOp,ALUOp,AluSrc,Branch,MemRead,MemWrite,RegWrite,MemtoReg,Reg2Loc);
+module Controller
+#(parameter delay=100)
+(insOp,ALUOp,AluSrc,Branch,MemRead,MemWrite,RegWrite,MemtoReg,Reg2Loc);
 	input [10: 0] insOp;
 	output reg [1:0]ALUOp;
 	output reg AluSrc;
@@ -26,7 +28,7 @@ module Controller(insOp,ALUOp,AluSrc,Branch,MemRead,MemWrite,RegWrite,MemtoReg,R
 		else if(insOp==11'b11111000010)
 			begin				
 			//LDUR
-				 Reg2Loc = 1'bx;
+				 Reg2Loc = 1'b0;
 			  	 AluSrc = 1'b1;
 			  	 MemtoReg = 1'b1;
 			  	 RegWrite = 1'b1;
@@ -41,24 +43,35 @@ module Controller(insOp,ALUOp,AluSrc,Branch,MemRead,MemWrite,RegWrite,MemtoReg,R
 			//STUR
 				 Reg2Loc = 1'b1;
 			  	 AluSrc = 1'b1;
-			  	 MemtoReg = 1'bx;
+			  	 MemtoReg = 1'b0;
 			  	 RegWrite = 1'b0;
 				 MemRead = 1'b0;
 			  	 MemWrite = 1'b1;
 				 Branch = 1'b0;
-				 ALUOp = 2'b10;
+				 ALUOp = 2'b00;
 			end
 		else if(insOp==11'b10110100xxx)
 			 begin 
 			// CBZ
 				 Reg2Loc = 1'b1;
 			  	 AluSrc = 1'b0;
-			  	 MemtoReg = 1'bx;
+			  	 MemtoReg = 1'b0;
 			  	 RegWrite = 1'b0;
 				 MemRead = 1'b0;
 			  	 MemWrite = 1'b0;
 				 Branch = 1'b1;
 				 ALUOp = 2'b01;
+			end
+		else
+			begin
+				Reg2Loc = 1'b0;
+			  	 AluSrc = 1'b0;
+			  	 MemtoReg = 1'b0;
+			  	 RegWrite = 1'b0;
+				 MemRead = 1'b0;
+			  	 MemWrite = 1'b0;
+				 Branch = 1'b0;
+				 ALUOp = 2'b00;
 			end
 	end	
 endmodule
