cde4bb9a572b2840782250ce55e15560d3c2d1f4
Lsaeedy98 <leila.saeedy1995@gmail.com>
2018-12-29 10:40:46 +0330

    edited sign extend

--- SignEx.v
+++ SignEx.v
@@ -5,7 +5,12 @@ output reg[63:0] ExOut;
 
 always@(ExIn) 
 begin
-    ExOut[31:0] = ExIn[31:0];
-    ExOut[63:32] = {32{ExIn[31]}};
+	ExOut=64'bz;
+	//conditional branch
+	if(ExIn[30]==1'b0)
+		ExOut={{45{ExIn[23]}},ExIn[23:5]};
+		//LDUR & STUR
+	else if(ExIn[30]==1'b1)
+		ExOut={{55{ExIn[23]}},ExIn[20:12]};
 end
 endmodule
